fec9e62df9e9da0cda21a5447c6c596fc18017cc
cscsonka <76667017+cscsonka@users.noreply.github.com>
2022-12-26 23:22:02 +0100

    commit

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 $("#hr-cont").on("click", () => {
     heartRateSensor.connect()
-        .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
+        .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(heartRateMeasurement => {
+            heartRateMeasurement.addEventListener("characteristicvaluechanged", event => {
+                var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
+                var hr = heartRateMeasurement.heartRate;
+                $("#hr-cont spam").html(hr);
+                if (hr < 150) {
+                    $("#hr-cont").css({ "background-color": "#5ccd5c" });
+                } else {
+                    $("#hr-cont").css({ "background-color": "inianred" });
+                }
+            });
+        }))
         .catch(error => {
-            $("hr-cont spam").html = error;
+            $("#hr-cont spam").html(error);
         });
 });
-
-function handleHeartRateMeasurement(heartRateMeasurement) {
-    heartRateMeasurement.addEventListener("characteristicvaluechanged", event => {
-        var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
-        var hr = heartRateMeasurement.heartRate;
-        $("hr-cont spam").html(hr);
-        if (hr < 150) {
-            $("hr-cont").css({ "background-color": "#5ccd5c" });
-        } else {
-            $("hr-cont").css({ "background-color": "inianred" });
-        }
-    });
-}
-
-
-
-
 
 
