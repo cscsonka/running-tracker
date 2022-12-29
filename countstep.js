c6ad202b612e0fafaa14535067eac231ede8e4b6
cscsonka <76667017+cscsonka@users.noreply.github.com>
2022-12-29 17:50:48 +0100

    commit

--- countstep.js
+++ countstep.js
@@ -1,41 +1,44 @@
 (function () {
-	function Countstep() {
+	function Step() {
 		this.init();
-		return this.count;
-
+		return this.Step;
 	}
-	Countstep.prototype = {
+
+	Step.prototype = {
 		init: function () {
 			var _this = this;
 			_this.flag = false;
-			_this.count = [];
-			_this.count[0] = 0;
+			_this.step = {
+				count: 0,
+				timestamp: null
+			};
+
 			function motionHandler(event) {
 				var accGravity = event.accelerationIncludingGravity;
 				_this.yg = accGravity.y;
 				return false;
 			}
+
 			function orientationHandler(event) {
 				if ((_this.yg - 10 * Math.sin(event.beta * Math.PI / 180)) > 1) {
 					_this.flag = true;
 				}
 				if ((_this.yg - 10 * Math.sin(event.beta * Math.PI / 180)) < -1) {
 					if (_this.flag == true) {
-						_this.count[0]++;
+						_this.step.count++;
+						_this.step.timestamp = new Date().getTime();
 						_this.flag = false;
-
 					};
 
 				}
 			}
+
 			if (window.DeviceMotionEvent && window.DeviceOrientationEvent) {
 				window.addEventListener("devicemotion", motionHandler, false);
 				window.addEventListener("deviceorientation", orientationHandler, false);
-				return _this.count;
+				return _this.step;
 			}
 		},
 	}
-	window.Countstep = Countstep;
+	window.Step = Step;
 })();
-
-
