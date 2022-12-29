75a245f89d39eb0067606689847d3bcb3f374ac8
cscsonka <76667017+cscsonka@users.noreply.github.com>
2022-12-29 11:50:26 +0100

    commit

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var hr_limit = 100;
-
 $("#hr-cont").on("click", () => {
     heartRateSensor.connect()
         .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(heartRateMeasurement => {
+            var hr_limit = 100;
+            var hr_limit_reached = false;
             heartRateMeasurement.addEventListener("characteristicvaluechanged", event => {
                 var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
                 var hr = heartRateMeasurement.heartRate;
                 $("#hr-cont spam")
-                    .css({ "font-size": "450px" })
+                    .css({ "font-size": "400px" })
                     .html(hr);
                 if (hr < hr_limit) {
                     $("#hr-cont").css({ "background-color": "#5ccd5c" });
-                    voice_feedback("Now it's fine.", 5);
+                    if (hr_limit_reached === true) voice_feedback("Now it's fine.", 5);
+                    hr_limit_reached = false;
                 } else {
                     $("#hr-cont").css({ "background-color": "indianred" });
-                    voice_feedback("Slow down!", 6);
+                    if (hr_limit_reached === false) voice_feedback("Slow down!", 6);
+                    hr_limit_reached = true;
                 }
             });
             document.getElementById("app").requestFullscreen();
