62d49afe83ddf6309b0ef0bd6f951db873a030e7
cscsonka <76667017+cscsonka@users.noreply.github.com>
2023-01-14 14:37:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<script>
-
 $(".loader").html('<svg viewBox="25 25 50 50"><circle cx="50" cy="50" r="20"/></svg>');
 $("#loader-init").delay(2000).fadeOut(300, () => $("#app").fadeIn(300));
 $.event.special.tap.tapholdThreshold = 3000;
@@ -82,66 +80,5 @@ function voice_feedback(text, voice_index) {
     speech.text = text;
     speechSynthesis.speak(speech);
 }
-
-
-
-/*
-var DATA = {};
-$("#hr-cont").on("click", () => {
-    heartRateSensor.connect()
-        .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(heartRateMeasurement => {
-            $("#hr-cont").off("click");
-            var hr_limit = 85;
-            var hr_limit_reached = false;
-            heartRateMeasurement.addEventListener("characteristicvaluechanged", event => {
-                var hr = heartRateSensor.parseHeartRate(event.target.value).heartRate;
-                $("#hr-cont spam").html(hr);
-                if (hr < hr_limit) {
-                    $("#hr-cont").css({ "background-color": "#5ccd5c" });
-                    if (hr_limit_reached === true) voice_feedback("Now it's fine.", 5);
-                    hr_limit_reached = false;
-                } else {
-                    $("#hr-cont").css({ "background-color": "indianred" });
-                    if (hr_limit_reached === false) voice_feedback("Slow down!", 6);
-                    hr_limit_reached = true;
-                }
-            });
-        }))
-        .catch(error => {
-            $("#hr-cont spam")
-                .css({ "font-size": "40px" })
-                .html(error.toString());
-        });
-});
-
-
-$("#cadence-cont").on("click", () => {
-    document.body.requestFullscreen();
-});
-
-
-function voice_feedback(text, voice_index) {
-    var speech = new SpeechSynthesisUtterance();
-    speech.voice = speechSynthesis.getVoices()[voice_index];
-    speech.text = text;
-    speechSynthesis.speak(speech);
-}
-
-
-var previous_ts;
-var my_step = new Step();
-window.addEventListener("devicemotion", function () {
-    var current_ts = my_step.step.timestamp;
-    if (previous_ts !== current_ts) {
-        var cadence = Math.round(60000 / (current_ts - previous_ts));
-        $("#cadence-cont spam").html(cadence);
-        previous_ts = current_ts;
-    }
-}, false);
-*/
-
-</script>
-
-
 
 
